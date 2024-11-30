8d49b171d94e358cce4e917d441fad377e559cef
Wendy Chen <jaewchen@gmail.com>
2024-11-30 17:24:55 +0000

    direnv options

--- home-manager/modules/direnv.nix
+++ home-manager/modules/direnv.nix
@@ -5,5 +5,11 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    config = {
+      hide_env_diff = true;
+      whitelist = {
+        prefix = [ "~/projects" ];
+      };
+    };
   };
 }
