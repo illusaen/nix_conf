aa4562f7ba09e7b084c2e7ed5270738f574017b0
Wendy Chen <jaewchen@gmail.com>
2025-01-15 03:40:02 +0000

    using mkoutofstoresymlink for all separate config files

--- home-manager/modules/link.nix
+++ home-manager/modules/link.nix
@@ -5,12 +5,12 @@ let
   linkDir = "${HM_MODULE_DIR}/linked";
 in
 {
-  home.file.".local/bin/scripts".source = link ./linked/scripts;
-  xdg.configFile."wezterm/wezterm.lua".source = link ./linked/wezterm.lua;
-  xdg.configFile."fish/conf.d/autols.fish".source = link ./linked/autols.fish;
-  xdg.configFile."fish/functions/dev-shell.fish".source = link ./linked/dev-shell.fish;
-  xdg.configFile."helix/config.toml".source = link ./linked/helix-config.toml;
-  xdg.configFile."helix/languages.toml".source = link ./linked/helix-languages.toml;
-  xdg.configFile."lvim/config.lua".source = link ./linked/lvim-config.lua;
-  xdg.configFile."starship.toml".source = link ./linked/starship.toml;
+  home.file.".local/bin/scripts".source = link "${linkDir}/scripts";
+  xdg.configFile."wezterm/wezterm.lua".source = link "${linkDir}/wezterm.lua";
+  xdg.configFile."fish/conf.d/autols.fish".source = link "${linkDir}/autols.fish";
+  xdg.configFile."fish/functions/dev-shell.fish".source = link "${linkDir}/dev-shell.fish";
+  xdg.configFile."helix/config.toml".source = link "${linkDir}/helix-config.toml";
+  xdg.configFile."helix/languages.toml".source = link "${linkDir}/helix-languages.toml";
+  xdg.configFile."lvim/config.lua".source = link "${linkDir}/lvim-config.lua";
+  xdg.configFile."starship.toml".source = link "${linkDir}/starship.toml";
 }
